6284afe24cbf1ab8abc52002bb6508aa04f78bf5
TonioSanchez <antonio.worms@gmail.com>
2017-08-08 08:40:12 -0300

    Class 16-17-18

---
+++ CLASE-18.sql
@@ -0,0 +1,9 @@
+
+
+CALL GetOfficeByCountry('USA');
+
+CALL CountOrderByStatus('Shipped',@total);
+SELECT @total;
+
+CALL CountOrderByStatus('in process',@total);
+SELECT @total AS  total_in_process;
